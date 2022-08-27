381e64df43d9a40069d72fba0bad90b10d44102f
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-27 09:47:24 +0530

    Create 02_selectors.js

---
+++ JS/02_selectors.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+
+    $('#box').css('border' , '2px solid black')
+    $('#list li:first').css('color' , 'red');
+    $('#list li:eq(3)').css('color' , 'blue');                       // eq is equation here 3 li is targeted  we can also use lt = lower than gt inplace of eq
+})
