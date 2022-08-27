99c4638f6447673a220b0ef67e4c25b1ba306c33
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-27 09:56:12 +0530

    GET method

---
+++ JS/07_get-method.js
@@ -0,0 +1,19 @@
+       $(document).ready(function(){
+            var a = $('#box').html();
+            console.log(a)
+        });
+
+        $(document).ready(function(){
+            var a = $('#box').text();
+            console.log(a)
+        });
+        
+        $(document).ready(function(){
+            var a = $('#box').attr('class');
+            console.log(a)
+        });
+
+        $(document).ready(function(){
+            var a = $('#box').attr('class');
+            console.log(a)
+        });
