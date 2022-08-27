baaefa21bb6312a7891ef3714f90a7bc7064597c
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-27 09:44:38 +0530

    This is basic syntax of jQuery

---
+++ JS/01_syntax.js
@@ -0,0 +1,9 @@
+        $(document).ready(function(){                  // when document get ready then this js file work
+            
+            // targeting id using #idname in $()
+            // targeting class using .classname in $()
+            // targeting tag using tagname in $()
+
+            var a = $('#test1').html();
+            console.log(a);
+        })
