a1939c64e4575031b13bd9c33ead95de7c3b5a41
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-27 09:54:16 +0530

    Create 06_window-events.js

---
+++ JS/06_window-events.js
@@ -0,0 +1,9 @@
+      $(document).ready(function () {
+            $(window).scroll(function () {
+                console.log("Youe are sascrolling");
+            });
+
+            $(window).load(function(){
+                console.log("Loading Content")
+            })
+        });
