c5ff7df85c1e5d506d38bd5fb5d97aa9abd2d363
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-28 12:38:19 +0530

    Create 09_css-class-method.js

---
+++ JS/09_css-class-method.js
@@ -0,0 +1,14 @@
+        $(document).ready(function(){
+
+            $('#addbutton').click(function(){
+                $('#box').addClass("first");
+            })
+  
+            $('#removebutton').click(function(){
+                $('#box').removeClass("first");
+            })
+
+            $('#togglebutton').click(function(){
+                $('#box').toggleClass("second");
+            })
+        })
