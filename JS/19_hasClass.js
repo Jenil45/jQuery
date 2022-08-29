5cb8fd7632e2f811aa525c326718693e22358466
Jenil45 <103255558+Jenil45@users.noreply.github.com>
2022-08-29 22:39:26 +0530

    Create 19_hasClass.js

---
+++ JS/19_hasClass.js
@@ -0,0 +1,8 @@
+        $(document).ready(function(){
+            
+
+            $('#hasClass').click(function(){
+                var a = $('#box').hasClass("test")                
+                console.log(a)
+            })
+        })
